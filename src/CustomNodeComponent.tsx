50106302e7d049e4734b1b0293e774006c3b3d30
syedmahmad <devm.ahmad@gmail.com>
2023-11-14 18:36:43 +0500

    added editor

--- src/CustomNodeComponent.tsx
+++ src/CustomNodeComponent.tsx
@@ -1,11 +1,38 @@
 /* eslint-disable */
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect, useRef } from 'react';
 import { Handle, Position } from 'reactflow';
-import { Box, Textarea, Editable, EditablePreview, EditableTextarea, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger } from '@chakra-ui/react';
+import {
+  Box,
+  Textarea,
+  Editable,
+  EditablePreview,
+  EditableTextarea,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+} from '@chakra-ui/react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+import DOMPurify from 'dompurify';
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 // import { Handle, Position } from 'reactflow';
 
 export default memo(({ data, isConnectable }: any) => {
-  let [value, setValue] = React.useState('')
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty(),
+  );
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  useEffect(() => {
+    let html = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(html);
+  }, [editorState]);
 
   const handleClick = (event) => {
     // saving node id to use it later when user try to create new ndoe
@@ -15,12 +42,12 @@ export default memo(({ data, isConnectable }: any) => {
 
   const onOpen = () => {
     console.log('open');
-  }
+  };
 
   const onClose = () => {
     console.log('close');
     // need to send API call to save flow
-  }
+  };
 
   return (
     <>
@@ -34,14 +61,14 @@ export default memo(({ data, isConnectable }: any) => {
         style={{ background: '#000' }}
         isConnectable={isConnectable}
       />
-      <Popover 
-        placement='left' 
-        isLazy
-        onOpen={onOpen}
-        onClose={onClose}>
+      <Popover placement="left" isLazy onOpen={onOpen} onClose={onClose}>
         <PopoverTrigger>
           <Box
-            style={{ background: '#fff', height: 'fit-content', width: '300px' }}
+            style={{
+              background: '#fff',
+              height: 'fit-content',
+              width: '300px',
+            }}
             onClick={handleClick}
           >
             <Box
@@ -60,22 +87,45 @@ export default memo(({ data, isConnectable }: any) => {
                 justifyContent="center"
                 alignItems="center"
               >
-                {value}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(convertedContent),
+                  }}
+                ></div>
               </Box>
             </Box>
             {/* <img src="https://via.placeholder.com/300.png/09f/fff" alt="Girl in a jacket" width="100" height="100" /> */}
-          </Box>  
+          </Box>
         </PopoverTrigger>
         <PopoverContent>
           <PopoverArrow />
           {/* <PopoverCloseButton /> */}
           <PopoverHeader>Text</PopoverHeader>
           <PopoverBody>
-            <Textarea 
-              placeholder='Add text...'
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              size='sm' />
+            <Editor
+              editorRef={(ref) => {
+                ref?.focus();
+              }}
+              editorState={editorState}
+              onEditorStateChange={setEditorState}
+              wrapperClassName="wrapper-class"
+              editorClassName="editor-class"
+              toolbarClassName="toolbar-class"
+              toolbar={{
+                options: ['inline', 'link'],
+                inline: {
+                  inDropdown: false,
+                  options: ['bold', 'italic', 'underline', 'strikethrough'],
+                },
+                link: {
+                  inDropdown: false,
+                  options: ['link'],
+                },
+              }}
+              toolbarCustomButtons={[
+                <div onClick={() => alert('Coming Soon!')}>Insert Entity</div>,
+              ]}
+            />
           </PopoverBody>
         </PopoverContent>
       </Popover>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -6,12 +5,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { ReactFlowProvider } from 'reactflow';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ReactFlowProvider>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </ReactFlowProvider>
-    ,
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <ReactFlowProvider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </ReactFlowProvider>,
+  // </React.StrictMode>,
 );
