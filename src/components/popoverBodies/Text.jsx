13d563cde1fb640033a4142c5323741617d993dd
syedmahmad <devm.ahmad@gmail.com>
2024-03-14 17:46:06 +0500

    fixed editor variants

--- src/components/popoverBodies/Text.jsx
+++ src/components/popoverBodies/Text.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */ 
 import React, { useState, useEffect } from "react";
 import {
   Box,
@@ -19,7 +20,156 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 const TextBody = React.forwardRef((props, ref) => {
   const [showCaptureResponse, setShowCaptureResponse] = useState(false);
   const { comp, components, setComp, type } = props;
-  const [convertedContent, setConvertedContent] = useState(comp.props.value === 'Add something here' ? '' : comp.props.value);
+  const [editorList, setEditorList] = useState(comp.props.value === 'Add something here' ? [comp.props.value] : [comp.props.value, ...comp.variants]);
+  const deleteNode = () => {
+    if (components.length === 1) {
+      alert("Node should contain at least 1 widget!");
+      return;
+    }
+    const newData = components.filter((data) => data.order !== comp.order);
+    setComp(newData);
+  };
+  return (
+    <>
+      <Box
+          display="flex"
+          justifyContent="space-between"
+          position="relative"
+      >
+        <Box
+            marginLeft="10px"
+            width="93%"
+        >
+         {/* Editor Component */}
+         {editorList.map((item,index) => <EditorComponent comp={comp} 
+         components={components} setComp={setComp} ref={ref} item={item}
+         setEditorList={setEditorList} index={index} 
+         editorList={editorList}
+
+         />)}
+        </Box>
+        <Box
+            cursor="pointer"
+            onClick={() => deleteNode()}
+            style={{height: 'fit-content'}}
+        >
+          <Icon
+              color='hsla(0, 0%, 85%, 1)'
+              icon="ic:outline-delete"
+          />
+        </Box>
+      </Box>
+
+      <Box
+          marginLeft="10px"
+          marginTop="10px"
+      >
+        <Checkbox
+            color="text.body"
+            iconColor='blue.400'
+            iconSize='1rem'
+            onChange={() => {setShowCaptureResponse(!showCaptureResponse);}}
+        >
+          {type === "customer_response_node" ?  "Capture response" :  "Question"}
+        </Checkbox>
+      </Box>
+
+      {type === "customer_response_node" && showCaptureResponse ? <>
+        <Box
+            marginLeft="10px"
+            marginTop="10px"
+            width="93%"
+        >
+          <Text
+              color="text.body"
+              fontSize="xs"
+          >
+            Triggered Intent
+          </Text>
+
+          <Input
+              borderRadius="0.3125rem"
+              placeholder="Intent Name"
+              size="sm"
+          />
+        </Box>
+
+        <Box
+            marginLeft="10px"
+            marginTop="10px"
+            width="93%"
+        >
+          <Text
+              color="text.body"
+              fontSize="xs"
+          >
+            Add Entity
+          </Text>
+
+          <Input
+              borderRadius="0.3125rem"
+              placeholder="Entity name"
+              size="sm"
+          />
+        </Box>
+      </> : null}
+
+      {type !== "customer_response_node" && (
+        <>
+          <Divider
+              borderColor="#D8D8D8"
+              marginLeft="10px"
+              marginTop="10px"
+              width="93%"
+          />
+
+          <Box
+              display="flex"
+              justifyContent="right"
+              marginTop="10px"
+              width="93%"
+          >
+            <Button
+                _hover={{ backgroundColor: 'primary.90' }}
+                backgroundColor="primary.100"
+                color="white"
+                onClick={() => {
+                  const newList = [...editorList, ''];
+                  setEditorList(newList)
+                }}
+                size="sm"
+                width="118px"
+            >
+              Add Variant
+            </Button>
+          </Box>
+        </>
+      )}
+    </>
+  )
+});
+
+export default TextBody;
+
+
+// function to add linrBreaks ...
+function replaceEmptyPTagWithBrTa(htmlString) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, 'text/html');
+  const paragraphs = doc.querySelectorAll('p');
+
+  paragraphs.forEach(paragraph => {
+    if (!paragraph.textContent.trim()) {
+      paragraph.innerHTML = '<br>';
+    }
+  });
+
+  return doc.body.innerHTML;
+}
+
+
+const EditorComponent = ({comp, components, setComp, ref, setEditorList, index, item, editorList }) => {
+  const [convertedContent, setConvertedContent] = useState(item);
   const [editorState, setEditorState] = useState(() => {
     const contentState = stateFromHTML(convertedContent);
     return EditorState.createWithContent(contentState);
@@ -32,30 +182,31 @@ const TextBody = React.forwardRef((props, ref) => {
       block.getInlineStyleAt(0).toJS();
     });
     const html = convertToHTML(editorState.getCurrentContent());
+    // setting new editor state 
+    let newList = editorList;
+    newList[index] = html;
+    setEditorList(newList);
     setConvertedContent(html);
   }, [editorState]);
 
   useEffect(() => {
+    const firstEditor = editorList[0];
+    // Remove the first item from editorList to only store the variants in the variants array.
+    const updatedEditorList = editorList.slice(1);
+    const variants = updatedEditorList.map((item) => item);
     const arr = components?.map((item) => {
       if (item.order === comp.order) {
         item.props = {
           ...item.props,
-          value: convertedContent
+          value: firstEditor
         };
+        item.variants = variants
       }
-      return item;
+      return item
     });
     setComp(arr);
   }, [convertedContent]);
 
-  const deleteNode = () => {
-    if (components.length === 1) {
-      alert("Node should contain at least 1 widget!");
-      return;
-    }
-    const newData = components.filter((data) => data.order !== comp.order);
-    setComp(newData);
-  };
 
   const moveSelectionToEnd = (editorState) => {
     const contentState = editorState.getCurrentContent();
@@ -220,172 +371,50 @@ useEffect(() => {
   setConvertedContent(html);
 }, [editorState]);
 
-
-  return (
+  return(
     <>
-      <Box
-          display="flex"
-          justifyContent="space-between"
-          position="relative"
+    <Editor
+    editorClassName="editor-class nopan nodrag"
+    editorState={editorState}
+    onEditorStateChange={setEditorState}
+    placeholder="Add text here"
+    ref={ref}
+    tabIndex={0}
+    toolbar={{
+      image: {
+        alt: { present: true, mandatory: false },
+        previewImage: true,
+        inputAccept: 'svg',
+      },
+      options: ['inline', 'link'],
+      inline: {
+        inDropdown: false,
+        options: ['bold', 'italic', 'underline', 'strikethrough'],
+      },
+      link: {
+        inDropdown: false,
+        options: ['link'],
+      },
+    }}
+    toolbarClassName="toolbar-class nopan nodrag"
+    toolbarCustomButtons={[
+      <div style={{
+      display: 'flex',
+      alignItems: 'center'
+    }}
       >
-        <Box
-            marginLeft="10px"
-            width="93%"
+        <div
+            className="insert-entity"
+            onClick={() => alert('Coming Soon!')}
         >
-          <Editor
-              editorClassName="editor-class nopan nodrag"
-              editorState={editorState}
-              onEditorStateChange={setEditorState}
-              placeholder="Add text here"
-              ref={ref}
-              tabIndex={0}
-              toolbar={{
-                image: {
-                  alt: { present: true, mandatory: false },
-                  previewImage: true,
-                  inputAccept: 'svg',
-                },
-                options: ['inline', 'link'],
-                inline: {
-                  inDropdown: false,
-                  options: ['bold', 'italic', 'underline', 'strikethrough'],
-                },
-                link: {
-                  inDropdown: false,
-                  options: ['link'],
-                },
-              }}
-              toolbarClassName="toolbar-class nopan nodrag"
-              toolbarCustomButtons={[
-                <div style={{
-                display: 'flex',
-                alignItems: 'center'
-              }}
-                >
-                  <div
-                      className="insert-entity"
-                      onClick={() => alert('Coming Soon!')}
-                  >
-                    Insert Entity
-                  </div>
-                </div>,
-            ]}
-              wrapperClassName="wrapper-class"
-          />
-        </Box>
-
-        <Box
-            cursor="pointer"
-            onClick={() => deleteNode()}
-            style={{height: 'fit-content'}}
-        >
-          <Icon
-              color='hsla(0, 0%, 85%, 1)'
-              icon="ic:outline-delete"
-          />
-        </Box>
-      </Box>
-
-      <Box
-          marginLeft="10px"
-          marginTop="10px"
-      >
-        <Checkbox
-            color="text.body"
-            iconColor='blue.400'
-            iconSize='1rem'
-            onChange={() => {setShowCaptureResponse(!showCaptureResponse);}}
-        >
-          {type === "customer_response_node" ?  "Capture response" :  "Question"}
-        </Checkbox>
-      </Box>
-
-      {type === "customer_response_node" && showCaptureResponse ? <>
-        <Box
-            marginLeft="10px"
-            marginTop="10px"
-            width="93%"
-        >
-          <Text
-              color="text.body"
-              fontSize="xs"
-          >
-            Triggered Intent
-          </Text>
-
-          <Input
-              borderRadius="0.3125rem"
-              placeholder="Intent Name"
-              size="sm"
-          />
-        </Box>
-
-        <Box
-            marginLeft="10px"
-            marginTop="10px"
-            width="93%"
-        >
-          <Text
-              color="text.body"
-              fontSize="xs"
-          >
-            Add Entity
-          </Text>
-
-          <Input
-              borderRadius="0.3125rem"
-              placeholder="Entity name"
-              size="sm"
-          />
-        </Box>
-      </> : null}
-
-      {type !== "customer_response_node" && (
-        <>
-          <Divider
-              borderColor="#D8D8D8"
-              marginLeft="10px"
-              marginTop="10px"
-              width="93%"
-          />
-
-          <Box
-              display="flex"
-              justifyContent="right"
-              marginTop="10px"
-              width="93%"
-          >
-            <Button
-                _hover={{ backgroundColor: 'primary.90' }}
-                backgroundColor="primary.100"
-                color="white"
-                onClick={() => alert('Coming Soon!')}
-                size="sm"
-                width="118px"
-            >
-              Add Variant
-            </Button>
-          </Box>
-        </>
-      )}
-    </>
+          Insert Entity
+        </div>
+      </div>,
+  ]}
+    wrapperClassName="wrapper-class"
+/>
+<br/>
+</>
   )
-});
-
-export default TextBody;
-
-
-// function to add linrBreaks ...
-function replaceEmptyPTagWithBrTa(htmlString) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(htmlString, 'text/html');
-  const paragraphs = doc.querySelectorAll('p');
-
-  paragraphs.forEach(paragraph => {
-    if (!paragraph.textContent.trim()) {
-      paragraph.innerHTML = '<br>';
-    }
-  });
-
-  return doc.body.innerHTML;
 }
+
